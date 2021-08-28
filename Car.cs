73cd41e61859f10013f260036d7c7285f4af5338
ABHI <abhishekitte11@gmail.com>
2021-08-28 22:34:15 +0530

    Added a car class

---
+++ Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp58
+{
+    class Car
+    {
+        string color = "red";
+        int model = 345737;
+
+        public void Drive()
+        {
+            Console.WriteLine("Driving");
+        }
+        public static void Brake()
+        {
+            Console.WriteLine("Brake");
+        }
+            static void Main(string[] args)
+            {
+                Car myObj = new Car();
+                myObj.Drive();
+                Car.Brake();
+            }
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp58
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.Drive();
+            Car.Brake();
+        }
+    }
+}
